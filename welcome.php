37b5f702099380ec0f3e48c6b4ab6a1c0d0f0914
emtroublemaker <83471925+emtroublemaker@users.noreply.github.com>
2022-01-10 17:19:55 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,10 +1,13 @@
 <html>
+    <head>
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
     <body>
     <style>.error{color:red;}
     body{
         background-color: skyblue;
     }
     div{
+        background-image: url('second.jpg');
         background-color: MediumSeaGreen;
         width: 300px;
         border: 150 skyblue;
@@ -12,10 +15,11 @@
         margin: 500px;
     }
     </style>
+        </head>
     <div align="center">
         Welcome <?php echo $_POST['name']?><br><br>
-        Your email address is: <?php echo $_POST['email']?><br><br>
-        You commented <?php echo $_POST['comment']?><br><br>
-         Your Gender <?php echo $_POST['gender']?><br><br>
+        Your email address is:- <?php echo $_POST['email']?><br><br>
+        You commented:- <?php echo $_POST['comment']?><br><br>
+         Your Gender:- <?php echo $_POST['gender']?><br><br>
     </body>
     </html>
